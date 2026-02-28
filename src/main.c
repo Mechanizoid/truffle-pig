d591ed8cf2e8b808f220e853e5c780cf7ba0abf5
Christopher Phoenix <hello@chrisphoenix.dev>
2026-02-28 16:57:20 -0700

    fix: point include directives to correct location of header files

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <glfw.h>
-#include "../include/glad/glad.h"
+#include <GLFW/glfw.h>
+#include "glad/glad.h"
 
 int main(void) {
 	printf("Hello Opengl world!\n");
 
 	/* implement code to open GLFW window here */
 
-	
+
 	return 0;
 }
