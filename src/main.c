ac5347278747366495074b9890caa0e8c636a936
Christopher Phoenix <hello@chrisphoenix.dev>
2026-03-01 01:28:44 -0700

    feat: open a GLFW window containing an OpenGL viewport
    Initialize GLFW and GLAD, then open a window displaying a black
    background. Resize the OpenGL viewport when the user changes the
    size of the window.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "config.h"
 
@@ -5,11 +6,52 @@
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
 
-int main(void) {
+/* function declarations */
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+int main(void)
+{
 	printf("Hello Opengl world!\n");
 
 	/* implement code to open GLFW window here */
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL",
+					      NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "Failed to create GLFW window\n");
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		fprintf(stderr, "Failed to initialize GLAD\n");
+		exit(EXIT_FAILURE);
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	/* Render loop */
+	while(!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	/* properly close the resources used by GLFW window */
+	glfwTerminate();
 
 	return 0;
+}
+
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+	glViewport(0, 0, width, height);
 }
